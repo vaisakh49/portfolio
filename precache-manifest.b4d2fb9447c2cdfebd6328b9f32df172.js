318ba81413a4492161ab60849a1392db8ef09c97
vaisakh <vaisakhtp133@gmail.com>
2021-08-28 18:01:59 +0530

    Updates

---
+++ precache-manifest.b4d2fb9447c2cdfebd6328b9f32df172.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f66af961fda185a678a2248fd92bd23",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "99f47179cbf3035b95e5",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7cea8df1905fef443428",
+    "url": "/portfolio/static/css/main.3654d45b.chunk.css"
+  },
+  {
+    "revision": "99f47179cbf3035b95e5",
+    "url": "/portfolio/static/js/2.905d9102.chunk.js"
+  },
+  {
+    "revision": "9371b595e371c0d4c4f99812c6aece96",
+    "url": "/portfolio/static/js/2.905d9102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cea8df1905fef443428",
+    "url": "/portfolio/static/js/main.073fa55f.chunk.js"
+  },
+  {
+    "revision": "5e4cd4c6f14baf3f4e46",
+    "url": "/portfolio/static/js/runtime-main.86f1c7e8.js"
+  },
+  {
+    "revision": "444e4d0d5c12657ed99f92a89109ab72",
+    "url": "/portfolio/static/media/Profile.444e4d0d.jpg"
+  }
+]);
